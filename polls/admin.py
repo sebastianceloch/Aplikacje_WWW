0c3e72a216ce62378a19f97daf0d73d0923f12d8
sebastianceloch <celejkontakt@gmail.com>
2023-10-23 13:35:38 +0200

    lab 3

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 
-from .models import ToDo, Task
-
-admin.site.register(ToDo)
-admin.site.register(Task)
+from .models import Person,Job
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    readonly_fields = ['date_added']
+    list_display = ['name', 'surrname', 'sex_type', 'date_added', 'view_job']
+    list_filter = ('job', 'date_added')
+    @admin.display(empty_value="Stanowisko (Brak)")
+    def view_job(self, obj):
+        return f'{obj.job.name} ({obj.job.id})'
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_filter = ('name',)
--- polls/models.py
+++ polls/models.py
@@ -1,14 +1,25 @@
 from django.db import models
 
-# Create your models here.
-class ToDo(models.Model):
-    name = models.CharField(max_length=200)
-    def __str__(self):
-        return self.name
-
-class Task(models.Model):
-    name = models.CharField(max_length=200)
+class Job(models.Model):
+    name = models.CharField(max_length=60, blank=False)
     description = models.TextField(null=True, blank=True)
-    todo = models.ForeignKey(ToDo, on_delete=models.CASCADE)
     def __str__(self):
-        return self.name
+        return f'{self.name}'
+class Person(models.Model):
+    # lista wartości do wyboru w formie krotek
+    class Sex(models.IntegerChoices):
+        MALE = 1
+        FEMALE = 2
+        OTHER = 3
+    name = models.CharField(max_length=60, blank=False)
+    surrname = models.CharField(max_length=60, blank=False)
+    # wskazanie listy poprzez przypisanie do parametru choices
+    sex_type = models.IntegerField(choices=Sex.choices)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.name} {self.surrname}'
+
+    class Meta:
+        ordering = ["surrname"]
