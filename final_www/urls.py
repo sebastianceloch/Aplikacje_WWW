c6738e9ec530ca2c6b4ca36b41de2cdc0228dc51
sebastianceloch <celejkontakt@gmail.com>
2023-12-04 12:33:34 +0100

    small changes xd

--- final_www/urls.py
+++ final_www/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import include, path
+
+from . import settings
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += [
     path('api-auth/', include('rest_framework.urls')),
